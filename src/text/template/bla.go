d62bba94607bc4f336587ada4a8985967cde637c
Jonas Wielage <jonas.wielage@sonarsource.com>
2025-06-03 09:08:36 +0200

    test

--- src/text/template/bla.go
+++ src/text/template/bla.go
@@ -1,35 +1,19 @@
+// Copyright 2011 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package template
+
 import (
-	"crypto"
-	"crypto/md5"
-	"crypto/sha1"
+	"fmt"
+	"io"
+
 	"golang.org/x/crypto/md4"
-	"golang.org/x/crypto/ripemd160"
 )
 
 func main() {
-	tmp := md4.New() // Noncompliant {{Make sure this weak hash algorithm is not used in a sensitive context here.}}
-	//     ^^^^^^^^^
-	ripemd160.New()                   // Noncompliant
-	crypto.Hash.New(crypto.MD4)       // Noncompliant
-	crypto.Hash.New(crypto.MD5)       // Noncompliant
-	crypto.Hash.New(crypto.SHA1)      // Noncompliant
-	crypto.Hash.New(crypto.RIPEMD160) // Noncompliant
-	md5.Sum(data)                     // Noncompliant
-	sha1.Sum(data)                    // Noncompliant
-
-	md4.New(someArgs)                     // Noncompliant
-	md5.Sum()                             // Noncompliant
-	md5.Sum(more, args)                   // Noncompliant
-	crypto.Hash.New(crypto.MD4, extraArg) // Noncompliant
-
-	md4.other()
-	Hash.New(crypto.MD4)
-	crypto.Hash.New(crypto.SHA256)
-	crypto.Hash.New(unknownValue)
-	crypto.Hash.New()
-
-	// Extra test case for code coverage of MethodCall
-	"test".string()
-	"test".string
-	test.string
+	h := md4.New()
+	data := "These pretzels are making me thirsty."
+	io.WriteString(h, data)
+	fmt.Printf("%x", h.Sum(nil))
 }
